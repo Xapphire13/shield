a94f8056ff6c225cfcadbd5450efc5f60f37989f
Xapphire13 <3392349+Xapphire13@users.noreply.github.com>
2025-08-07 01:05:54 -0700

    remove duplicate OTC input RSX

--- app/src/pages/login.rs
+++ app/src/pages/login.rs
@@ -1,6 +1,8 @@
 use crate::{
-    api::AuthApi, app::Route, components::PrimaryButton, hooks::use_api_client,
-    utils::focus_element,
+    api::AuthApi,
+    app::Route,
+    components::{PrimaryButton, otp_input::OtpInput},
+    hooks::use_api_client,
 };
 use dioxus::prelude::*;
 
@@ -10,27 +12,7 @@ pub fn Login() -> Element {
     let client = use_api_client();
     let mut code = use_signal(String::new);
 
-    let handle_input = |digit: u8| {
-        move |ev: FormEvent| {
-            if let Ok(value) = ev.value().parse::<u32>() {
-                let prev: String = code()
-                    .chars()
-                    .take(digit.saturating_sub(1) as usize)
-                    .collect();
-                let next: String = format!("{prev}{value}").chars().take(6).collect();
-                let next_digit = next.len() + 1;
-
-                code.set(next);
-
-                if next_digit > 6 {
-                    let _ = focus_element("otp-submit-button");
-                } else {
-                    let next_input_id = format!("otp-digit-{next_digit}");
-                    let _ = focus_element(&next_input_id);
-                }
-            }
-        }
-    };
+    let handle_code_changed = use_callback(move |new_code| code.set(new_code));
 
     let handle_submit = use_callback(move |_| {
         let client = client.clone();
@@ -46,63 +28,11 @@ pub fn Login() -> Element {
         });
     });
 
-    let code = code();
-    let mut digits = code.chars();
-
     rsx! {
         div { class: "login-container",
             div { class: "login-card",
                 div { class: "otp-heading", "Enter OTP Code" }
-                div { class: "otp-input-container",
-                    input {
-                        id: "otp-digit-1",
-                        r#type: "text",
-                        "data-bwignore": "true",
-                        autocomplete: "off",
-                        oninput: handle_input(1),
-                        value: digits.next().map(|c| c.to_string()).unwrap_or(String::new()),
-                    }
-                    input {
-                        id: "otp-digit-2",
-                        r#type: "text",
-                        "data-bwignore": "true",
-                        autocomplete: "off",
-                        oninput: handle_input(2),
-                        value: digits.next().map(|c| c.to_string()).unwrap_or(String::new()),
-                    }
-                    input {
-                        id: "otp-digit-3",
-                        r#type: "text",
-                        "data-bwignore": "true",
-                        autocomplete: "off",
-                        oninput: handle_input(3),
-                        value: digits.next().map(|c| c.to_string()).unwrap_or(String::new()),
-                    }
-                    input {
-                        id: "otp-digit-4",
-                        r#type: "text",
-                        "data-bwignore": "true",
-                        autocomplete: "off",
-                        oninput: handle_input(4),
-                        value: digits.next().map(|c| c.to_string()).unwrap_or(String::new()),
-                    }
-                    input {
-                        id: "otp-digit-5",
-                        r#type: "text",
-                        "data-bwignore": "true",
-                        autocomplete: "off",
-                        oninput: handle_input(5),
-                        value: digits.next().map(|c| c.to_string()).unwrap_or(String::new()),
-                    }
-                    input {
-                        id: "otp-digit-6",
-                        r#type: "text",
-                        "data-bwignore": "true",
-                        autocomplete: "off",
-                        oninput: handle_input(6),
-                        value: digits.next().map(|c| c.to_string()).unwrap_or(String::new()),
-                    }
-                }
+                OtpInput { value: "", on_change: handle_code_changed, on_submit: handle_submit }
                 PrimaryButton { id: "otp-submit-button", on_press: handle_submit, "Submit" }
             }
         }

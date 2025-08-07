ecbb24d46139754a6cef919860febcad9aa5d382
Xapphire13 <3392349+Xapphire13@users.noreply.github.com>
2025-08-06 22:53:48 -0700

    debug vs release BASE_URL

--- app/src/use_api_client.rs
+++ app/src/use_api_client.rs
@@ -4,10 +4,15 @@ use dioxus::prelude::*;
 
 use crate::api_client::ApiClient;
 
+#[cfg(debug_assertions)]
+const BASE_URL: &str = "http://{}:3000";
+#[cfg(not(debug_assertions))]
+const BASE_URL: &str = "http://{}/api";
+
 pub fn use_api_client_provider(on_unauthorized: impl Fn() + 'static) {
     use_context_provider(|| {
         let hostname = web_sys::window().unwrap().location().hostname().unwrap();
-        let base_url = format!("http://{hostname}:3000");
+        let base_url = BASE_URL.replace("{}", &hostname);
         let client = ApiClient::new(base_url, on_unauthorized);
 
         Rc::new(client)
